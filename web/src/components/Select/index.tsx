a162ed0081b5753ca553c635c1a5ff1854123623
Lucas César <lucasambrogi1@hotmail.com>
2020-08-06 00:56:16 -0300

    TeacherForm page done.

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -16,6 +16,7 @@ const Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
         <div className="select-block">
             <label htmlFor={name}>{label}</label>
             <select id={name} {...rest}>
+                <option value="" disabled selected hidden>Seleciona uma opção</option>
                 {options.map(option => (
                     <option key={option.value} value={option.value}>{option.label}</option>
                 ))}
