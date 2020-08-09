6ba70d49bfce686117f1b2c99549cddc4694f974
Lucas César <lucasambrogi1@hotmail.com>
2020-08-08 23:45:28 -0300

    Created TeacherItem component.

---
+++ mobile/src/components/TeacherItem/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+import styles from './styles';
+
+const TeacherItem: React.FC = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.profile}>
+                <Image style={styles.avatar} source={{ uri: 'http://github.com/diego3g.png' }} />
+
+                <View style={styles.profileInfo}>
+                    <Text style={styles.name}>Diego Fernandes</Text>
+                    <Text style={styles.subject}>Química</Text>
+                </View>
+            </View>
+
+            <Text style={styles.bio}>
+                Uma biografia existente apenas para teste e sem funcionalidade alguma, somente existindo e apenas isso.
+            </Text>
+
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                    Preço/hora {'   '}
+                    <Text style={styles.priceValue}>R$ 20,00</Text>
+                </Text>
+
+                <View style={styles.buttonsContainer}>
+                    <RectButton style={styles.favoriteButton}>
+                        <Image source={heartOutlineIcon} />
+                    </RectButton>
+
+                    <RectButton style={styles.contactButton}>
+                        <Image source={whatsappIcon} />
+                        <Text style={styles.contactButtonText}>Entrar em contato</Text>
+                    </RectButton>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+export default TeacherItem;
